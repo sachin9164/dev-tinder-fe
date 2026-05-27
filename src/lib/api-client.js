353e4c6a5355969758df87b3f5a70c4a6c7cdddd
Sachin Charantmath <sachin@example.com>
2026-05-27 19:06:42 +0530

    refactor: remove console log for API base URL

--- src/lib/api-client.js
+++ src/lib/api-client.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const API_BASE_URL =
-
   `${window.location.protocol}//${window.location.host}`;
-console.log('API Base URL:', API_BASE_URL);
+
 export const apiClient = axios.create({
   baseURL: `${API_BASE_URL}/api`,
   withCredentials: true,
